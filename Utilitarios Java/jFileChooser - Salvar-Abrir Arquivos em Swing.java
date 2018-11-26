18712a5d3d08936133fd1b59e87804cf75aa8aae
willbigas <williambmauro@hotmail.com>
2018-11-26 08:50:47 -0200

    Pastas Ajustadas - Temas e Controle de utilitarios

---
+++ Utilitarios Java/jFileChooser - Salvar-Abrir Arquivos em Swing.java
@@ -0,0 +1,30 @@
+ /**
+     * UTILITARIOS SWING *
+     */
+    /**
+     * Janela de Salvar - JFileChooser - Para salvar arquivos em geral.
+     */
+//        
+//        JFileChooser chooser = new JFileChooser();
+//        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+//                "Arquivo Csv - Excel 2007 ou Inferior", "csv");
+//        chooser.setFileFilter(filter);
+//        int returnVal = chooser.showSaveDialog(this);
+//        if (returnVal == JFileChooser.APPROVE_OPTION) {
+//            String nomeArquivo = chooser.getSelectedFile().getName();
+//            String enderecoArquivo = chooser.getSelectedFile().getPath();
+    
+    
+    
+    /**
+     * Janela de Abrir - JFileChooser - Para abrir arquivos em geral.
+     */
+//        
+//        JFileChooser chooser = new JFileChooser();
+//        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+//                "Arquivo Csv - Excel 2007 ou Inferior", "csv");
+//        chooser.setFileFilter(filter);
+//        int returnVal = chooser.showOpenDialog(this);
+//        if (returnVal == JFileChooser.APPROVE_OPTION) {
+//            String nomeArquivo = chooser.getSelectedFile().getName();
+//            String enderecoArquivo = chooser.getSelectedFile().getPath();
