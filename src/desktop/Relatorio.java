9a3fc51debaff41ac012a38b799b10be230d0307
William Bigas Mauro <williambmauro@hotmail.com>
2019-05-31 21:33:43 -0300

    added relatorio.java

---
+++ src/desktop/Relatorio.java
@@ -0,0 +1,22 @@
+package desktop;
+
+/**
+ *
+ * @author Will
+ */
+public class Relatorio {
+    
+//    private static Connection con = Conexao.getConexao(); herdar conexão com o banco
+
+//    public static void chamarRelatorio(String enderecoArq) {
+//        JasperPrint jasperPrint = null;
+//        try {
+//            jasperPrint = JasperFillManager.fillReport(enderecoArq, null, con);
+//        } catch (Exception e) {
+//            JOptionPane.showMessageDialog(null, "Erro ao gerar relatório /n " + e);
+//        }
+//        JasperViewer.viewReport(jasperPrint , false); // false para não fechar a aplicação quando fechar o relatório.
+//        
+//    }
+    
+}
