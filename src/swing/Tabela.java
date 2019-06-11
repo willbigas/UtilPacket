f778cba8b7a47634798556ae09a8f9f0dba85e93
willbigas <williambmauro@hotmail.com>
2019-06-11 14:08:24 -0300

    Feature - Adicionados novos metodos em Tabela.

--- src/swing/Tabela.java
+++ src/swing/Tabela.java
@@ -5,6 +5,7 @@
  */
 package swing;
 
+import java.awt.Point;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -12,8 +13,52 @@ import javax.swing.table.DefaultTableCellRenderer;
 /**
  *
  * @author agostinho.junior
+ * @author william.mauro
  */
 public class Tabela {
+
+    private static DefaultTableCellRenderer centralizado = new DefaultTableCellRenderer();
+    private static DefaultTableCellRenderer esquerda = new DefaultTableCellRenderer();
+    private static DefaultTableCellRenderer direita = new DefaultTableCellRenderer();
+
+    public static void criandoAlinhamentosDaJtable() {
+        centralizado.setHorizontalAlignment(SwingConstants.CENTER);
+        esquerda.setHorizontalAlignment(SwingConstants.LEFT);
+        direita.setHorizontalAlignment(SwingConstants.RIGHT);
+    }
+
+    /**
+     * Centraliza o conteudo da tabela
+     *
+     * @param tableModel
+     * @param coluna
+     */
+    public static void centralizarConteudo(JTable tableModel, Integer coluna) {
+        criandoAlinhamentosDaJtable();
+        tableModel.getColumnModel().getColumn(coluna).setCellRenderer(centralizado);
+    }
+
+    /**
+     * Alinha o conteudo da tabela a direita
+     *
+     * @param tableModel
+     * @param coluna
+     */
+    public static void alinharConteudoADireita(JTable tableModel, Integer coluna) {
+        criandoAlinhamentosDaJtable();
+        tableModel.getColumnModel().getColumn(coluna).setCellRenderer(direita);
+    }
+
+    /**
+     * Alinha o conteudo da tabela a esquerda
+     *
+     * @param tableModel
+     * @param coluna
+     */
+    public static void alinharConteudoAEsquerda(JTable tableModel, Integer coluna) {
+        criandoAlinhamentosDaJtable();
+        tableModel.getColumnModel().getColumn(coluna).setCellRenderer(esquerda);
+    }
 
     /**
      * Redefinir largura da tabela
@@ -23,17 +68,8 @@ public class Tabela {
      * @param largura
      */
     public static void redimensionar(JTable tableModel, Integer coluna, Integer largura) {
-        autoRedimensionamentoDesligado(tableModel);
-        tableModel.getColumnModel().getColumn(coluna).setPreferredWidth(largura);
-    }
-
-    /**
-     * Desligando o auto redimensionamento
-     *
-     * @param tableModel
-     */
-    public static void autoRedimensionamentoDesligado(JTable tableModel) {
         tableModel.setAutoResizeMode(tableModel.AUTO_RESIZE_OFF);
+        tableModel.getColumnModel().getColumn(coluna).setPreferredWidth(largura);
     }
 
     /**
@@ -53,4 +89,16 @@ public class Tabela {
     public static void limparSelecaoDaTabela(JTable tableModel) {
         tableModel.getSelectionModel().clearSelection();
     }
+
+    public static int pegaLinhaComDoubleClick(java.awt.event.MouseEvent evt) {
+        JTable table = (JTable) evt.getSource();
+        Point point = evt.getPoint();
+        int row = table.rowAtPoint(point);
+        if (evt.getClickCount() == 2 && table.getSelectedRow() != -1) {
+            return row;
+        } else {
+            return -1;
+        }
+    }
+
 }
